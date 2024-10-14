11387d0004c57f91034c24f079fbf288691ed93a
sheraz61 <hsheraz271@gmail.com>
2024-10-14 21:36:01 +0500

    Create Context and complete the functions of add update delete and toggle

---
+++ src/Context/todoContext.js
@@ -0,0 +1,21 @@
+import { createContext,useContext } from "react";
+
+export const todoContext=createContext({
+todos:[
+    {
+        id:1,
+        msg:'Todo Msg',
+        completed:false
+    }
+],
+addTodo:(todo)=>{},
+updateTodo:(id,todo)=>{},
+deleteTodo:(id)=>{},
+toggleComplete:(id)=>{}
+})
+
+export const useTodo =()=>{
+    return useContext(todoContext)
+}
+
+export const todoProvider=todoContext.Provider
