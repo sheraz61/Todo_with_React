5f63e77ec48fa9c881087cf90e4681f644b2a6c6
sheraz61 <hsheraz271@gmail.com>
2024-10-14 21:42:52 +0500

    use local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -14,6 +14,19 @@ function App() {
   const toggleComplete = (id) => {
     setTodos((prev) => prev.map((prevtodo) => prevtodo.id === id ? { ...prevtodo, completed: !prevtodo.completed } : prevtodo))
   }
+
+
+  // fetch data from user local storage
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem('todos'))
+    if (todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  }, [])
+  // save data to user local storage
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
   return (
     <>
     </>
